d1c7a2da27c878fd753cecba071e27c77998ed18
JOE <joewttx@163.com>
2018-11-25 16:31:39 +0800

    配置文件

--- models/conn.go
+++ models/conn.go
@@ -2,10 +2,20 @@ package models
 
 import (
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
+	dbhost := beego.AppConfig.String("dbhost")
+    dbport := beego.AppConfig.String("dbport")
+    dbuser := beego.AppConfig.String("dbuser")
+    dbpassword := beego.AppConfig.String("dbpassword")
+    dbname := beego.AppConfig.String("dbname")
+    if dbport == "" {
+        dbport = "3306"
+    }
+    dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:root@tcp(localhost:8889)/joeblog?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", dsn)
 }
